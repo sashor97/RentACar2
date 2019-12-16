03f9d5793f2419173c23da9b157234544ddcb7c1
MetodiNakov123 <nakovmetodi181@gmail.com>
2019-12-17 00:37:22 +0100

    Add full reservation and show comments

--- RentACar/Controllers/RezervacijasController.cs
+++ RentACar/Controllers/RezervacijasController.cs
@@ -22,6 +22,148 @@ namespace RentACar.Controllers
             return View(rezervacii.ToList());
         }
 
+        // GET: Rezervacijas/CreateForVozilo/1
+        public ActionResult CreateForVozilo(int id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            //ViewBag.KorisnikId = new SelectList(db.Korisnici, "KorisnikId", "Name");
+            string email = User.Identity.GetUserName();
+
+            var Korisnici = db.Korisnici.Where(k => k.email == email);
+            var count = db.Korisnici.Where(k => k.email == email).Count();
+            if (count == 0)
+            {
+                return RedirectToAction("Create", "Korisniks");
+            }
+
+            List<int> ids = new List<int>();
+            ids.Add(id);
+
+            
+            ViewBag.VoziloId = new SelectList(ids);
+
+            return View();
+        }
+
+        // POST: Rezervacijas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateForVozilo(AddReservation model)
+        {
+
+            Rezervacija rezervacija = new Rezervacija();
+           // rezervacija.denoviIznajmuvanje = model.denoviIznajmuvanje;
+            rezervacija.DateFrom = Convert.ToDateTime(model.DateFrom);
+            rezervacija.DateTo = Convert.ToDateTime(model.DateTo);
+            rezervacija.VoziloId = model.VoziloId;
+            string dif = (rezervacija.DateTo - rezervacija.DateFrom).TotalDays.ToString();
+            rezervacija.denoviIznajmuvanje = Convert.ToInt32(dif);
+
+            var vozilo = db.Vozila.Find(model.VoziloId);
+            double cena = vozilo.PriceDay;
+
+            rezervacija.total = cena * rezervacija.denoviIznajmuvanje;
+
+
+            string email = User.Identity.GetUserName();
+            var korisnici = db.Korisnici.Where(k => k.email == email).First();
+            rezervacija.KorisnikId = korisnici.KorisnikId;
+
+
+
+            var rezervacii = db.Rezervacii.Where(m => m.VoziloId == model.VoziloId);
+
+            foreach(var r in rezervacii)
+            {
+                DateTime datumOd = r.DateFrom;
+                DateTime datumDo = r.DateTo;
+
+                DateTime momentalenDatumOd = rezervacija.DateFrom;
+                DateTime momentalenDatumDo = rezervacija.DateTo;
+
+                int result1 = DateTime.Compare(datumOd,momentalenDatumOd);
+                int result2 = DateTime.Compare(datumDo, momentalenDatumOd);
+
+                if (result1 <= 0 && result2 >= 0)
+                {
+                    List<int> ids = new List<int>();
+                    ids.Add(model.VoziloId);
+
+
+                    ViewBag.VoziloId = new SelectList(ids);
+
+                    return View();
+                }
+
+                int result3 = DateTime.Compare(datumOd, momentalenDatumDo);
+                int result4 = DateTime.Compare(datumDo, momentalenDatumDo);
+
+                if (result3 <= 0 && result4 >= 0)
+                {
+                    List<int> ids = new List<int>();
+                    ids.Add(model.VoziloId);
+
+
+                    ViewBag.VoziloId = new SelectList(ids);
+
+                    return View();
+                }
+
+                int result5 = DateTime.Compare(datumOd, momentalenDatumOd);
+                int result6 = DateTime.Compare(datumDo, momentalenDatumDo);
+
+                if (result5 >= 0 && result6 <= 0)
+                {
+                    List<int> ids = new List<int>();
+                    ids.Add(model.VoziloId);
+
+
+                    ViewBag.VoziloId = new SelectList(ids);
+
+                    return View();
+                }
+            }
+
+
+
+
+
+
+            db.Rezervacii.Add(rezervacija);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+            //   rezervacija.denoviIznajmuvanje = 10;
+            //   rezervacija.uspesnost = true;
+            //   rezervacija.plateno = true;
+            //   rezervacija.total = 1000;
+
+            //   if (ModelState.IsValid)
+            //   {
+            //       db.Rezervacii.Add(rezervacija);
+            //       db.SaveChanges();
+            //       return RedirectToAction("Index");
+            //   }
+
+            //   ViewBag.KorisnikId = new SelectList(db.Korisnici, "KorisnikId", "Name", rezervacija.KorisnikId);
+            //   ViewBag.VoziloId = new SelectList(db.Vozila, "VoziloId", "ModelName", rezervacija.VoziloId);
+            //   return View(rezervacija);
+            return Content("Hello");
+        }
+
+
+
+
+
+
         // GET: Rezervacijas/Details/5
         public ActionResult Details(int? id)
         {
@@ -29,7 +171,7 @@ namespace RentACar.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Rezervacija rezervacija = db.Rezervacii.Find(id);
+            Rezervacija rezervacija = db.Rezervacii.Include(m => m.Korisnik).Include(m => m.Vozilo).Where(m => m.RezervacijaId == id).First();
             if (rezervacija == null)
             {
                 return HttpNotFound();
@@ -63,6 +205,12 @@ namespace RentACar.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "RezervacijaId,denoviIznajmuvanje,uspesnost,plateno,total,VoziloId,KorisnikId,DateFrom,DateTo")] Rezervacija rezervacija)
         {
+
+           // rezervacija.denoviIznajmuvanje = 10;
+           // rezervacija.uspesnost = true;
+           // rezervacija.plateno = true;
+           // rezervacija.total = 1000;
+
             if (ModelState.IsValid)
             {
                 db.Rezervacii.Add(rezervacija);
@@ -82,7 +230,7 @@ namespace RentACar.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Rezervacija rezervacija = db.Rezervacii.Find(id);
+            Rezervacija rezervacija = db.Rezervacii.Include(m => m.Korisnik).Include(m => m.Vozilo).Where(m => m.RezervacijaId == id).First(); ;
             if (rezervacija == null)
             {
                 return HttpNotFound();
@@ -117,7 +265,7 @@ namespace RentACar.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Rezervacija rezervacija = db.Rezervacii.Find(id);
+            Rezervacija rezervacija = db.Rezervacii.Include(m => m.Korisnik).Include(m=> m.Vozilo).Where(m => m.RezervacijaId == id).First();
             if (rezervacija == null)
             {
                 return HttpNotFound();
---
+++ RentACar/Models/AddReservation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentACar.Models
+{
+    public class AddReservation
+    {
+        //public int denoviIznajmuvanje { get; set; }
+
+        public int VoziloId { get; set; }
+     
+        public string DateFrom { get; set; }
+        
+        public string DateTo { get; set; }
+
+       
+    }
+}
